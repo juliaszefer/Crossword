56a8f7404e0047b978c9ae3d3fb013594fe75595
Julia Szefer <julia.szefer@gmail.com>
2023-04-05 11:47:52 +0200

    sign in option and file encoding

--- Person.py
+++ Person.py
@@ -1,5 +1,29 @@
 class Person:
     def __init__(self, nick, email, password):
-        self.nick = nick
-        self.email = email
-        self.password = password
+        self.nick = self.encode(nick)
+        self.email = self.encode(email)
+        self.password = self.encode(password)
+
+    def __str__(self):
+        return f"{self.nick},{self.email},{self.password}"
+
+    def listtostring(self, strr):
+        tmp = ""
+        for i in strr:
+            tmp += i
+        return tmp
+
+    def encode(self, word):
+        key1 = "abcdefghijklmnoprstuvwxyz123456789"
+        key2 = "0987126543qplaksmznxjdiwuerydtfghu"
+        key1list = list(key1)
+        key2list = list(key2)
+        tochange = list(word)
+        for i in range(len(key1list)):
+            for j in range(len(tochange)):
+                if key1list[i] == tochange[j]:
+                    tochange[j] = key2list[i]
+        word = self.listtostring(tochange)
+        return word
+
+
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from Person import Person
 
+path = "data/logins.txt"
+
 
 def login():
     odp = input("Do you want to\n1. sign in\n2. log in")
+    users = readfile(path)
     if int(odp) == 1:
         nickname, email, password = input("Enter your nickname: "), input("Enter your e-mail: "),\
                                     input("Enter your password: ")
+        for p in users:
+            czygit = True
+            while czygit:
+                if nickname == p.nick:
+                    print("Nickname already exists, try again.")
+                    nickname = input("Enter your nickname: ")
+                else:
+                    czygit = False
+            czygit = True
+            while czygit:
+                if email == p.email:
+                    print("Email already exists, try again.")
+                    email = input("Enter your e-mail: ")
+                else:
+                    czygit = False
+
+        person = Person(nickname, email, password)
+        savechanges(path, person)
 
 
-def readfile(path):
+def readfile(pathh):
     arr = list()
-    ffile = open(path, 'r')
+    ffile = open(pathh, 'r')
     for line in ffile:
         arr.append(sortline(line))
     return arr
@@ -22,6 +43,7 @@ def sortline(v_line):
     person1 = Person(a, b, c)
     return person1
 
-def savechanges(path, persona):
-    ffile = open(path, 'w')
-    ffile.append(persona)
+
+def savechanges(pathh, persona):
+    ffile = open(pathh, 'a')
+    ffile.write(persona)
